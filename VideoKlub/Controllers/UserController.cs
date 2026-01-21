f2cdcc121e458bd170caf3ddb530aef1b4b2182a
zelenyzub <136793603+zelenyzub@users.noreply.github.com>
2026-01-21 13:49:19 +0100

    ux/ui fixes no1

--- VideoKlub/Controllers/UserController.cs
+++ VideoKlub/Controllers/UserController.cs
@@ -29,8 +29,8 @@ namespace VideoKlub.Controllers
         public async Task<IActionResult> Enable(string id)
         {
             await _userRepository.EnableUserAsync(id);
-            TempData["UserMessage"] = "Korisnik je aktiviran.";
-            TempData["UserType"] = "info";
+            TempData["FavoriteMessage"] = "Korisnik je aktiviran.";
+            TempData["FavoriteType"] = "info";
             return RedirectToAction("Index", "Dashboard");
         }
 
@@ -39,8 +39,8 @@ namespace VideoKlub.Controllers
         public async Task<IActionResult> Disable(string id)
         {
             await _userRepository.DisableUserAsync(id);
-            TempData["UserMessage"] = "Korisnik je deaktiviran.";
-            TempData["UserType"] = "info";
+            TempData["FavoriteMessage"] = "Korisnik je deaktiviran.";
+            TempData["FavoriteType"] = "info";
             return RedirectToAction("Index", "Dashboard");
         }
 
@@ -57,8 +57,18 @@ namespace VideoKlub.Controllers
         public async Task<IActionResult> ChangeRole(string userId, string newRole)
         {
             await _userRepository.ChangeUserRoleAsync(userId, newRole);
-            TempData["UserMessage"] = "Uloga korisnika je promenjena.";
-            TempData["UserType"] = "info";
+
+            string role = null;
+            if(newRole == "Admin")
+            {
+                role = "Administrator";
+            }
+            else
+            {
+                role = "Korisnik";
+            }
+            TempData["FavoriteMessage"] = "Uloga korisnika je promenjena u " + role;
+            TempData["FavoriteType"] = "info";
             return RedirectToAction("Index", "Dashboard");
         }
 
